a855364611aa4400e3aacbb6cb6c690202ae4987
davepi23 <davepi23@zoho.eu>
2018-11-22 14:08:35 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# ci-cd-pipeline-with-bitrise-s095785" 
---
+++ app/src/main/java/swolfs/david/pm_cicd_testproject/Utils.java
@@ -0,0 +1,8 @@
+package swolfs.david.pm_cicd_testproject;
+
+public class Utils {
+    public static int addNumbers(int first, int second) {
+        return first + second;
+    }
+
+}
